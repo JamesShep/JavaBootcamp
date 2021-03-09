e348e69e86ba0ad013a7005225fa13ec70b37c25
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-03-09 17:15:21 +1000

    Week 11.3 - More Unit Test Examples

--- Week11/test/com/coderscampus/lesson1/CustomArrayListTest.java
+++ Week11/test/com/coderscampus/lesson1/CustomArrayListTest.java
@@ -6,31 +6,81 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CustomArrayListTest {
 
-    // Test-Driven Development Methodology
+    // Test-driven Development Methodology
     // Step 1 - Write a failing test
-    // Step 2 - Write the business logic to make the test pass
+    // Step 2 - Write the business logic to make the test pass (code itself)
     // Step 3 - Refactor your code
+    // -- Repeat
 
     @Test
-    void should_add_one_item_to_list() {
+    void should_add_one_item_to_list () {
         // Three A's
-        // Arrange, Act, Assert
-
         // Arrange
-        CustomArrayList<Integer> customList = new CustomArrayList<>();
-
         // Act
-        customList.add(10);
-        Integer expectedResult = customList.get(0);
-        Integer expectedSize = customList.getSize();
-
         // Assert
+
+        // ARRANGE
+        CustomList<Integer> sut = new CustomArrayList<>();
+
+        // ACT
+        sut.add(10);
+        Integer expectedResult = sut.get(0);
+        Integer expectedSize = sut.getSize();
+
+        // ASSERT
         // What would we expect to happen?
-        // I expect there to be an Integer with value 10 stored in the first
-        // index in my customList
+        // I expect there to be an integer with value 10 stored in the first
+        // index in my custom list
 
         assertEquals(10, expectedResult);
         assertEquals(1, expectedSize);
     }
 
+    @Test
+    void should_add_eleven_items_to_list () {
+        // Three A's
+        // Arrange
+        // Act
+        // Assert
+
+        // ARRANGE
+        // sut == System under test
+        CustomList<Integer> sut = new CustomArrayList<>();
+
+        // ACT
+        for (int i = 1; i <= 11; i++ ) {
+            sut.add(i);
+        }
+
+        // ASSERT
+
+        for (int i = 0; i < 11; i++) {
+            assertEquals(i + 1, sut.get(i));
+        }
+        assertEquals(11, sut.getSize());
+    }
+
+    @Test
+    void should_add_twentyone_items_to_list () {
+        // Three A's
+        // Arrange
+        // Act
+        // Assert
+
+        // ARRANGE
+        // sut == System under test
+        CustomList<Integer> sut = new CustomArrayList<>();
+
+        // ACT
+        for (int i = 1; i <= 21; i++ ) {
+            sut.add(i);
+        }
+
+        // ASSERT
+
+        for (int i = 0; i < 21; i++) {
+            assertEquals(i + 1, sut.get(i));
+        }
+        assertEquals(21, sut.getSize());
+    }
 }
