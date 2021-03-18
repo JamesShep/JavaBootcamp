b1fb9ab0ebb87523b18d8307637043bad3ced418
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-03-18 22:41:20 +1000

    Week 12.3 - Understanding Asynchronous Programming

--- Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -4,10 +4,16 @@ public class MultiThreadingApp {
 
     public static void main(String[] args) {
 
+        String message = "Starting";
+        System.out.println(message);
+        System.out.println(Thread.currentThread().getName());
+
         for (int i=0; i<5; i++) {
             SomeTask task = new SomeTask();
             new Thread(task).start();
-
         }
+
+        message = "Done";
+        System.out.println(message);
     }
 }
