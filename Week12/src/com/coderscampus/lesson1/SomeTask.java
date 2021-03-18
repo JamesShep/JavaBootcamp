556bebc708f3b5f3dc2f4d53e040b5b8e6726c37
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-03-18 23:07:47 +1000

    Week 12.4 - Levering Executors

--- Week12/src/com/coderscampus/lesson1/SomeTask.java
+++ Week12/src/com/coderscampus/lesson1/SomeTask.java
@@ -4,15 +4,19 @@ import java.util.Random;
 
 public class SomeTask implements Runnable {
 
+    private static int taskId = 0;
+
     @Override
     public void run() {
+
         for (int i=0; i<10000000; i++) {
             Integer int1 = new Random().nextInt();
             Integer int2 = new Random().nextInt();
             Double something = (int1 + int2) * 1.5;
         }
-        System.out.println("Hey look at me, i'm a task! " +
-                "I'm running on thread: " + Thread.currentThread().getName());
+        taskId++;
+        System.out.println("Hey look at me, i'm a task-id: " + taskId +
+                "! I'm running on thread: " + Thread.currentThread().getName());
 
     }
 }
