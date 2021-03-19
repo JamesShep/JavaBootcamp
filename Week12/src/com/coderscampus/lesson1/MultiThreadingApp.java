1a5a33e114cee782c99f418d33e0c60cd3752bae
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-03-19 23:54:18 +1000

    Week 12.8 - The default ForkJoinPool with CompletableFutures

--- Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -1,5 +1,7 @@
 package com.coderscampus.lesson1;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class MultiThreadingApp {
@@ -24,15 +26,24 @@ public class MultiThreadingApp {
         // For a CPU bound operation like we have in our "SomeTask" class,
         // we should make use of an ExecutorService
 
-        // ExecutorService service = Executors.newSingleThreadExecutor();
+        List<CompletableFuture<Void>> tasks = new ArrayList<>();
+
+        // This is how you get access to the ForkJoinPool's common pool,
+        // Which is the default thread pool that's used with the CompletableFutures
+        // if you do not specify an executor
+
+        // ExecutorService executor = Executors.newSingleThreadExecutor();
+
+         // ExecutorService executor = Executors.newSingleThreadExecutor();
 
         for (int i=0; i<20; i++) {
-            CompletableFuture.supplyAsync(SomeTask::new)
+            CompletableFuture<Void> task = CompletableFuture.supplyAsync(SomeTask :: new)
+            // CompletableFuture.supplyAsync(SomeTask::new, executor)
+            // CompletableFuture.supplyAsync(SomeTask::new)
                     .thenApply(SomeTask::call)
                     .thenAccept(System.out::println);
 
-
-
+            tasks.add(task);
             // Futures were great prior to java 8, but now we have something better
 
             // Future<TaskDto> futureTask = service.submit(new SomeTask());
@@ -44,5 +55,11 @@ public class MultiThreadingApp {
 
         message = "Done";
         System.out.println(message);
+
+        while (tasks.stream()
+                    .filter(CompletableFuture::isDone)
+                    .count() < 20) {
+            // This just loops and keeps the main thread alive until all threads are done working
+        }
     }
 }
