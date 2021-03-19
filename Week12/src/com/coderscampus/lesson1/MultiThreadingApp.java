f061a44d34ea506b03954de16416f60124c60420
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-03-19 21:02:36 +1000

    Week 12.7 - CompletableFutures in Java

--- Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -1,9 +1,6 @@
 package com.coderscampus.lesson1;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class MultiThreadingApp {
 
@@ -27,11 +24,20 @@ public class MultiThreadingApp {
         // For a CPU bound operation like we have in our "SomeTask" class,
         // we should make use of an ExecutorService
 
-        ExecutorService service = Executors.newSingleThreadExecutor();
+        // ExecutorService service = Executors.newSingleThreadExecutor();
 
-        for (int i=0; i<50; i++) {
-            Future<TaskDto> futureTask = service.submit(new SomeTask());
-            System.out.println(futureTask.get());
+        for (int i=0; i<20; i++) {
+            CompletableFuture.supplyAsync(SomeTask::new)
+                    .thenApply(SomeTask::call)
+                    .thenAccept(System.out::println);
+
+
+
+            // Futures were great prior to java 8, but now we have something better
+
+            // Future<TaskDto> futureTask = service.submit(new SomeTask());
+
+            // System.out.println(futureTask.get());
             // is a blocking task / call to the get method
             // going to stop and wait for the future task/wrapper, for the value
         }
