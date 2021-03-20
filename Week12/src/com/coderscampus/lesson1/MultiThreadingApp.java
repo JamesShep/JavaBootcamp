99083ca63d70b1b399c1fdf03fb45b2f31e28b55
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-03-20 21:43:19 +1000

    Week 12.9 - Using a Custom Executor with CompletableFutures

--- Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -26,40 +26,22 @@ public class MultiThreadingApp {
         // For a CPU bound operation like we have in our "SomeTask" class,
         // we should make use of an ExecutorService
 
-        List<CompletableFuture<Void>> tasks = new ArrayList<>();
-
         // This is how you get access to the ForkJoinPool's common pool,
         // Which is the default thread pool that's used with the CompletableFutures
         // if you do not specify an executor
 
-        // ExecutorService executor = Executors.newSingleThreadExecutor();
+        ExecutorService cpuBoundTask = Executors.newFixedThreadPool(3);
+        ExecutorService ioBoundTask = Executors.newCachedThreadPool();
 
-         // ExecutorService executor = Executors.newSingleThreadExecutor();
 
-        for (int i=0; i<20; i++) {
-            CompletableFuture<Void> task = CompletableFuture.supplyAsync(SomeTask :: new)
-            // CompletableFuture.supplyAsync(SomeTask::new, executor)
-            // CompletableFuture.supplyAsync(SomeTask::new)
-                    .thenApply(SomeTask::call)
-                    .thenAccept(System.out::println);
-
-            tasks.add(task);
-            // Futures were great prior to java 8, but now we have something better
-
-            // Future<TaskDto> futureTask = service.submit(new SomeTask());
-
-            // System.out.println(futureTask.get());
-            // is a blocking task / call to the get method
-            // going to stop and wait for the future task/wrapper, for the value
+        for (int i = 0; i < 20; i++) {
+            CompletableFuture.supplyAsync(SomeTask::new)
+                    .thenApplyAsync(SomeTask::doSomeWork, cpuBoundTask)
+                    .thenApplyAsync(SomeTask::markComplete, ioBoundTask)
+                    .thenAcceptAsync(System.out::println, ioBoundTask);
         }
 
         message = "Done";
         System.out.println(message);
-
-        while (tasks.stream()
-                    .filter(CompletableFuture::isDone)
-                    .count() < 20) {
-            // This just loops and keeps the main thread alive until all threads are done working
-        }
     }
 }
--- Week12/src/com/coderscampus/lesson1/SomeTask.java
+++ Week12/src/com/coderscampus/lesson1/SomeTask.java
@@ -3,13 +3,13 @@ package com.coderscampus.lesson1;
 import java.util.Random;
 import java.util.concurrent.Callable;
 
-public class SomeTask implements Callable<TaskDto> {
+public class SomeTask {
 
     private static int taskId = 0;
+    private TaskDto taskDto;
 
-    @Override
-    public TaskDto call() {
-        TaskDto taskDto = new TaskDto();
+    public SomeTask doSomeWork() {
+        taskDto = new TaskDto();
         for (int i = 0; i < 10000000; i++) {
             Integer int1 = new Random().nextInt();
             Integer int2 = new Random().nextInt();
@@ -19,6 +19,11 @@ public class SomeTask implements Callable<TaskDto> {
         taskId++;
         System.out.println("Hey look at me, i'm a task-id: " + taskId +
                 "! I'm running on thread: " + Thread.currentThread().getName());
+        taskDto.setFinished(true);
+        return this;
+    }
+
+    public TaskDto markComplete () {
         taskDto.setFinished(true);
         return taskDto;
     }
