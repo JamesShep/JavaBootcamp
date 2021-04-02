bc8fc0a938733ae8a73ec46dfae0c9bfc8a62c36
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-04-02 23:19:55 +1000

    Week 13.3 - GetMapping and PostMapping Annotations

--- Week13/src/main/java/com/coderscampus/week13/web/FileController.java
+++ Week13/src/main/java/com/coderscampus/week13/web/FileController.java
@@ -3,6 +3,7 @@ package com.coderscampus.week13.web;
 import com.coderscampus.week13.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -14,10 +15,20 @@ public class FileController {
     @Autowired
     private FileService fileService;
 
-    @GetMapping("")
+    @GetMapping("/read-lines")
     public List<String> readLines () throws IOException {
         fileService = new FileService();
         return fileService.readFile("test.txt");
-
     }
+
+    @GetMapping("/hello-world")
+    public String helloWorld () {
+        return  "Hello World!";
+    }
+
+    @PostMapping("/hello-world")
+    public String helloWorldPost () {
+        return "okay cool, you just posted some data!";
+    }
+
 }
