323376399036e6ad84d0a96ff8313f2f3cc3326b
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-04-14 18:50:33 +1000

    Week 13.4 - Application Context and Singletons

--- Week13/src/main/java/com/coderscampus/week13/web/FileController.java
+++ Week13/src/main/java/com/coderscampus/week13/web/FileController.java
@@ -1,23 +1,52 @@
 package com.coderscampus.week13.web;
 
 import com.coderscampus.week13.service.FileService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class FileController {
 
     @Autowired
-    private FileService fileService;
+    private ApplicationContext applicationContext;
+
+    /*@Autowired
+    private FileService fileService;*/
+
+    @GetMapping("/customer-report")
+    public Integer getCustomerData (Integer customerId) {
+        Integer customerData = 0;
+        // fetch data based on customerId
+        customerData = getCustomerDataById(customerId);
+        System.out.println("I think you're beautiful");
+        return customerData;
+    }
+
+    private Integer getCustomerDataById(Integer customerId) {
+        return new Random().nextInt();
+    }
 
     @GetMapping("/read-lines")
     public List<String> readLines () throws IOException {
-        fileService = new FileService();
+        FileService fileService = applicationContext.getBean(FileService.class);
+        System.out.println(fileService);
+        fileService = applicationContext.getBean(FileService.class);
+        System.out.println(fileService);
+        fileService = applicationContext.getBean(FileService.class);
+        System.out.println(fileService);
+        fileService = applicationContext.getBean(FileService.class);
+        System.out.println(fileService);
+        fileService = applicationContext.getBean(FileService.class);
+        System.out.println(fileService);
         return fileService.readFile("test.txt");
     }
 
