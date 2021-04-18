81be77b623eb1e95437cbcdc221285ca3e53fcbd
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-04-18 20:08:47 +1000

    Week 14.1 - JSON

---
+++ Week14/src/main/java/com/coderscampus/Week14/JsonExample.java
@@ -0,0 +1,40 @@
+package com.coderscampus.Week14;
+
+public class JsonExample {
+
+    /**
+     *
+     * JSON
+     * Javascript Object Notation
+     * A simple String based way to notate data
+     * - Key Value Pairs
+     * - Key : Value pairs are separated by commas
+     * - Objects are represented by { }
+     * Arrays are represented by [ ]
+     *
+     * Important Note:  JSON is NOT tied to JavcScript... it is
+     * language agnostic (It will work in any language)
+     *
+     * Person : name, age, gender
+     *
+     * {
+     *  "name" : "James S",
+     *  "age" : 28,
+     *  "gender" : "male"
+     *  }
+     *
+     *  List<Person> people
+     *  [
+         *  {
+         *      "name" : "James S",
+         *      *  "age" : 28,
+         *      *  "gender" : "male"
+         *  },
+         *  {
+         *      "name" : "Jane Doe",
+         *      *  "age" : 22,
+         *      *  "gender" : "female"
+         *  }
+     *  ]
+    */
+}
