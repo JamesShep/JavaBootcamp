02e72899060a2742c7394cabe37effa96c1105ee
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-04-20 15:58:39 +1000

    Week 14.7 - Mocking a Repository

--- Week14/src/main/java/com/coderscampus/Week14/dto/Person.java
+++ Week14/src/main/java/com/coderscampus/Week14/dto/Person.java
@@ -1,6 +1,10 @@
 package com.coderscampus.Week14.dto;
 
+import java.util.Objects;
+
 public class Person {
+
+    private long id;
     private String name;
     private String gender;
     private Integer age;
@@ -29,12 +33,35 @@ public class Person {
         this.age = age;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
     @Override
     public String toString() {
         return "Person{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", gender='" + gender + '\'' +
                 ", age=" + age +
                 '}';
     }
+
 }
--- Week14/src/main/java/com/coderscampus/Week14/web/PersonController.java
+++ Week14/src/main/java/com/coderscampus/Week14/web/PersonController.java
@@ -1,26 +1,33 @@
 package com.coderscampus.Week14.web;
 
 import com.coderscampus.Week14.dto.Person;
+import com.coderscampus.Week14.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PersonController {
 
+    @Autowired
+    private PersonRepository personRepository;
+
     @PostMapping("/persons")
-    public Person createPerson (@RequestParam String name, String gender, Integer age) {
+    public Person createPerson (@RequestParam String name, String gender, Integer age, Long id) {
         Person person = new Person();
         person.setName(name);
         person.setAge(age);
         person.setGender(gender);
+        person.setId(id);
+        personRepository.save(person);
         return person;
     }
 
     // fetches the person with ID 1
     // http://localhost:8080/persons/1
-    @GetMapping("/persons/{personID}")
+    @GetMapping("/persons/{personId}")
     public Person getPerson (@PathVariable Long personId) {
         System.out.println("Person ID is: " + personId);
-        return null;
+        return personRepository.findById(personId);
 
     }
 }
