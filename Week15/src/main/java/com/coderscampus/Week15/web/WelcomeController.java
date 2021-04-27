dff95419bb2aec2f9b2bdb79eb8942a99d4c8c42
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-04-27 17:54:35 +1000

    Week 15.3 - PostMapping with a Regular Controller

--- Week15/src/main/java/com/coderscampus/Week15/web/WelcomeController.java
+++ Week15/src/main/java/com/coderscampus/Week15/web/WelcomeController.java
@@ -1,8 +1,10 @@
 package com.coderscampus.Week15.web;
 
+import com.coderscampus.Week15.domain.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WelcomeController {
@@ -12,10 +14,21 @@ public class WelcomeController {
         // This is what gets returned / resolved when we return a string
         // inside a regular controller GetMapping method
         // src / main / resources / templates / {filename}.html
-        String firstName = "James";
+
+        Person person = new Person();
+
+        model.put("person", person);
+
+        /*String firstName = "James";
         String lastName = "S";
         model.put("firstName", firstName);
-        model.put("lastName", lastName);
+        model.put("lastName", lastName);*/
+        return "welcome";
+    }
+
+    @PostMapping("")
+    public String postRootWebpage (Person person) {
+        System.out.println(person);
         return "welcome";
     }
 }
