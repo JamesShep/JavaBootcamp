289be7f161a2e139a5d476c1e1d4c23b8e662751
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-05-18 23:46:33 +1000

    Week 18.3 - Mapping an Entity

--- Week18/src/main/java/com/coderscampus/week18/hibernateexample/domain/User.java
+++ Week18/src/main/java/com/coderscampus/week18/hibernateexample/domain/User.java
@@ -1,10 +1,15 @@
 package com.coderscampus.week18.hibernateexample.domain;
 
+import javax.persistence.*;
+
+@Entity // Class name = User, DB table name = user
+@Table(name = "users")
 public class User {
 
     private String username;
     private String password;
     private String name;
+    private Long userId;
 
     public String getUsername() {
         return username;
@@ -28,5 +33,14 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getUserId() {
+        return userId;
     }
 }
